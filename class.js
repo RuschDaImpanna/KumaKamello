d71b3a1968dce591fed959f8c6524f89dd4d909d
Rusch Da Impanna <ruschdaimpanna@gmail.com>
2025-12-16 12:33:06 -0500

    Update to class settings tab

--- class.js
+++ class.js
@@ -1,18 +1,31 @@
-const classController = []
-let nextClassId = 0
+//Get the panel container
+const container = document.getElementById('classManagement')
+
+//Class container objects
+let classController = []
+
+document.addEventListener("change", () => {
+
+    console.log(classController)
+
+})
 
 function addClass(){
 
+    let nextClassId = classController[classController.length-1] === undefined ? 0:classController[classController.length-1].id+1
+
     const basicStructure = {
 
-        id : nextClassId++,
-        title : 'Class'+ String(nextClassId - 1).padStart(2, '0'),
+        id : nextClassId,
+        title : 'Class'+ String(nextClassId).padStart(2, '0'),
         color : getRandomColor(),
         firstLength : 2,
         secondLenght : 0,
         splitBlock : false
 
     }
+
+    nextClassId++
 
     function getRandomColor() {
 
@@ -29,6 +42,7 @@ function addClass(){
 
     classController.push(basicStructure)
 
+    //Assign a panel for a classController object
     createPanel(classController[classController.length-1])
 
     console.log(classController)
@@ -37,77 +51,154 @@ function addClass(){
 
 function createPanel(classData) {
 
-    //Get the panel container
-    const container = document.getElementById('classManagement')
-
     //Create div
     const panel = document.createElement('div')
-    panel.classList.add('class-panel')
+    panel.classList.add('classPanel')
     panel.dataset.id = classData.id
 
     //Header
     const header = document.createElement('div')
     header.classList.add('panelHeader')
 
-    //Title
-    const title = document.createElement('h3')
-    title.innerText = classData.title
+        //Title
+        const title = document.createElement('h3')
+        title.innerText = classData.title
 
-    //Delete button
-    const deleteBtn = document.createElement('button')
-    deleteBtn.innerText = 'Delete'
-    deleteBtn.onclick = () => deleteClass(classData.id) //Delete function
+        //Delete button
+        const deleteBtn = document.createElement('button')
+        deleteBtn.innerText = 'Delete'
+        deleteBtn.onclick = () => deleteClass(classData.id) //Delete function
 
-    header.append(title, deleteBtn)
+        //Push and title to header
+        header.append(title, deleteBtn)
 
     //Settings
     const controls = document.createElement('div')
     controls.classList.add('panelControls')
 
 
-    //Title input
-    const nameInput = document.createElement('input')
-    nameInput.type = 'text'
-    nameInput.value = classData.title
+        //Title input
+        const nameInput = document.createElement('input')
+        nameInput.type = 'text'
+        nameInput.value = classData.title
+        nameInput.placeholder = 'Class title'
 
-    nameInput.oninput = () => {
+        //OnInput, changes on real time
+        nameInput.oninput = () => {
 
-        classController[classData.id].title = nameInput.value
-        title.innerText = nameInput.value
+            //Rewrite title at classController object
+            classController[classData.id].title = nameInput.value
+            //Dynamically, change text on real time
+            title.innerText = nameInput.value
 
-    }
+        }
 
-    //Color input
-    const colorInput = document.createElement('input')
-    colorInput.type = 'color'
-    colorInput.value = classData.color
+        //Color input
+        const colorInput = document.createElement('input')
+        colorInput.type = 'color'
+        colorInput.value = classData.color
 
-    colorInput.oninput = () => {
-        classController[classData.id].color = colorInput.value
-    }
+        colorInput.oninput = () => {
 
-    //Append every setting
-    controls.append(
-        nameInput,
-        colorInput,
-        /*sizeInput,
-        splitInput*/
-    )
+            //Rewrite title at classController object
+            classController[classData.id].color = colorInput.value
+
+        }
+
+        //Fancy label for the first lenght
+        const fstLngthH3 = document.createElement('h3')
+        fstLngthH3.innerText = 'Class unit lenght'
+        fstLngthH3.id = 'Fh3_' + classData.id
+
+        //First hour lenght
+        const firstLengthInput = document.createElement('input')
+        firstLengthInput.type = 'number'
+        firstLengthInput.min = 1
+        firstLengthInput.max = 3
+        firstLengthInput.value= classData.firstLength
+
+        firstLengthInput.oninput = () => {
+
+            //Rewrite first lenght at classController object
+            classController[classData.id].firstLength = firstLengthInput.value
+
+        }
+
+        //Fancy label for the second lenght
+        const sndLngthH3 = document.createElement('h3')
+        sndLngthH3.innerText = 'Second class segment lenght'
+        sndLngthH3.id = 'Sh3_' + classData.id
+
+        //First hour lenght
+        const secondLengthInput = document.createElement('input')
+        secondLengthInput.type = 'number'
+        secondLengthInput.min = 1
+        secondLengthInput.max = 3
+        secondLengthInput.value= classData.firstLength
+
+        secondLengthInput.oninput = () => {
+
+            //Rewrite first lenght at classController object
+            classController[classData.id].secondLength = secondLengthInput.value
+
+        }
+
+        //Splitted class (two classes per weer)
+        const splitInput = document.createElement('input')
+        splitInput.type = 'checkbox'
+        splitInput.id = 'splitFor' + classData.id
+        splitInput.checked = classData.splitBlock
+
+        //The label too
+        const splitLabel = document.createElement('label')
+        splitLabel.htmlFor = 'splitFor'+ classData.id
+        splitLabel.innerText = 'Splitted class (seen twice a week)'
+
+        splitInput.onclick = () => {
+
+            //Rewrite split at classController object
+            classController[classData.id].splitBlock = splitInput.checked
+
+            if(classController[classData.id].splitBlock) {
+
+                document.getElementById('Fh3_' + classData.id).innerText = 'First class segment lenght'
+
+            } else {
+
+                document.getElementById('Fh3_' + classData.id).innerText = 'Class unit lenght'
+
+            }
+
+
+        }
+        
+
+        //Push every setting
+        controls.append(
+            nameInput,
+            colorInput,
+            fstLngthH3,
+            firstLengthInput,
+            splitInput,
+            splitLabel
+        )
 
     //Header + controls
     panel.append(header, controls)
+
     //Ship it
     container.appendChild(panel)
 
 }
 
+//This is for deleting a class at the delete class button
 function deleteClass(id) {
 
     // Array delete
-    classController.splice(id, 1)
+    classController = classController.filter(del => del.id !== id)
 
     // Panel delete
-    const panel = document.querySelector(`.class-panel[data-id="${id}"]`)
+    const panel = document.querySelector(`.classPanel[data-id="${id}"]`)
     if (panel) panel.remove()
 
     console.log(classController)
