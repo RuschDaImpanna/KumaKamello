8f849d3c6c6da615f188fde897d0df7c8a9adfc1
Rusch Da Impanna <ruschdaimpanna@gmail.com>
2025-12-22 10:44:29 -0500

    Creating ghost block for moving blocks

--- movingBlocks.js
+++ movingBlocks.js
@@ -3,17 +3,21 @@ import { classController } from "./class.js";
 
 document.addEventListener("updateBlock", () => {
 
-    classController.forEach(classObj => {
+    classController.forEach((element, i) => {
 
-        const block = document.getElementById('block' + classObj.id)
+        const block = document.getElementById('block' + element.id)
 
-        makeDraggable(block)
+
+        makeDraggable(block, i)
 
     })
 
 })
 
-function makeDraggable (block) {
+function makeDraggable (block, id) {
+
+    const voucherTop = block.querySelector('.voucherTop')
+    const voucherBottom = block.querySelector('.voucherBottom')
 
     let isDragging = false
 
@@ -25,7 +29,8 @@ function makeDraggable (block) {
     let offsetX = 0
     let offsetY = 0
 
-    block.addEventListener('mousedown', onMouseDown)
+    voucherTop?.addEventListener('mousedown', onMouseDown)
+    voucherBottom?.addEventListener('mousedown', onMouseDown)
 
     function onMouseDown (e) {
 
@@ -39,28 +44,107 @@ function makeDraggable (block) {
         const rect = block.getBoundingClientRect()
 
         //Get mouse position
-        startX = e.clientX
-        startY = e.clientY
+        startX = e.pageX
+        startY = e.pageY
 
         //Position by mouse and block
-        offsetX = startX - rect.left
-        offsetY = startY - rect.top
+        offsetX = startX - rect.left - window.scrollX
+        offsetY = startY - rect.top - window.scrollY
+
+        //Create ghost block
+        const ghostBlock = createGhost(block, classController[id].color, classController[id].title)
+        const blocksContainer = document.querySelector('.blocks')
+
+        blocksContainer.insertBefore(ghostBlock, block)
+
+        //Move to body so doesn't have a parent
+        document.body.appendChild(block, voucherTop)
 
         //Make class styles
         block.classList.add('dragging')
 
         block.style.position = 'absolute'
 
+        block.style.left = rect.left + 'px'
+        block.style.top  = rect.top + 'px'
+
         block.style.width = rect.width + 'px'
         block.style.height = rect.height + 'px'
 
         block.style.zIndex = 1000
 
-        block.style.opacity = 0.7 
+        block.style.opacity = 0.7
 
-        console.log(true)
+
+        //While grabbing, you can move or drop
+        document.addEventListener('mousemove', onMouseMove)
+        document.addEventListener('mouseup', onMouseUp)
 
     }
+
+    function onMouseMove(e) {
+
+        if (!isDragging) return
+
+        block.style.left = `${e.pageX - offsetX}px`
+        block.style.top = `${e.pageY - offsetY}px`
+
+        ghostMove(ghostBlock, block, blocksContainer)
+
+    }
+
+    function onMouseUp() {
+
+        //Stop dragging
+        isDragging = false
+        block.classList.remove('dragging')
+        block.style.zIndex = ''
+        block.style.opacity = 1
+
+        document.removeEventListener('mousemove', onMouseMove)
+        document.removeEventListener('mouseup', onMouseUp)
+
+        snapBackToBlocks(block)
+
+    }
+
+    function createGhost (block, color, title) {
+
+        //Create ghost
+        const ghost = document.createElement('div')
+        ghost.classList.add('ghost')
+
+        ghost.style.height = block.offsetHeight + 'px'
+        
+        ghost.style.border = '3px dashed ' + color
+        ghost.style.borderRadius = '5px'
+
+        ghost.style.display = 'flex'
+        ghost.style.justifyContent = 'center'
+        ghost.style.alignItems = 'center'
+
+            //Create text
+            const ghostText = document.createElement('h3')
+            ghostText.innerText = title
+            ghostText.style.color = color
+
+            ghostText.style.textAlign = 'center'
+
+        ghost.append(ghostText)
+
+
+        return ghost
+
+    }
+
+    function snapBackToBlocks(block) {
+
+        block.style.position = 'relative'
+        block.style.left = ''
+        block.style.top = ''
+
+    }
+
 
 
 }
